a62a02f5313c8d45f41e60e737281c6102b80c42
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-11-18 14:05:25 +1000

    improved output products, add js fuction add_to_cart & get_cnt_products

--- public/app.js
+++ public/app.js
@@ -1,6 +1,33 @@
-var x = 1;
 
 function something() {
+
+	var x = window.localStorage.getItem('x');
+
+	x = x * 1 + 1;
+
 	alert(x);
-	x = x + 1;
+
+	window.localStorage.setItem('x', x)
+	
 }
+
+function add_to_cart(product_id) {
+	var key = 'product_' + product_id
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
+}
+
+function get_cnt_products() {
+	var total = 0;
+	for (var i = 0; i < window.localStorage.length; i++) {
+
+		key = window.localStorage.key(i);
+		total = total + window.localStorage.getItem(key) * 1;
+	}
+	alert('В корзине ' + total + ' товаров');
+}
+
+
+
+
